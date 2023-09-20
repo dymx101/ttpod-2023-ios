de69b99ebec362b7d22e1a041e3b1d3824bdcfa2
Yiming Dong <dymx101@hotmail.com>
2023-09-20 15:41:00 +0800

    initial commit

---
+++ TTPodMusicApp/TTPodMusicApp/TTPodMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTPodMusicAppApp.swift
+//  TTPodMusicApp
+//
+//  Created by yiming on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TTPodMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
